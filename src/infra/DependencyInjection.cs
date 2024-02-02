603637f4d02a25c0aaab9b37a37f2f554ffdb4ee
felix2000jp <felix2000jp@gmail.com>
2024-02-02 13:47:40 +0000

    fix

--- src/infra/DependencyInjection.cs
+++ src/infra/DependencyInjection.cs
@@ -29,6 +29,9 @@ public static class DependencyInjection
             throw new ArgumentException("Data context cannot be null");
         }
 
-        context.Database.Migrate();
+        if (context.Database.CanConnect())
+        {
+            context.Database.Migrate();
+        }
     }
 }
