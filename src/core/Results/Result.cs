c6609b982328fb70bf38bca20f814fdc0314bf22
felix2000jp <felix2000jp@gmail.com>
2024-03-24 11:21:44 +0000

    inherit result

--- src/core/Results/Result.cs
+++ src/core/Results/Result.cs
@@ -1,32 +1,6 @@
 namespace core.Results;
 
-public readonly record struct Result : IResult
-{
-    private readonly ResultValue? _value;
-    private readonly ResultError? _error;
-
-    private Result(bool isError, ResultValue? value = default, ResultError? error = default)
-    {
-        IsError = isError;
-        _error = error;
-        _value = value;
-    }
-
-    public bool IsError { get; }
-    public ResultValue Value => _value ?? throw new InvalidOperationException("Value was expected but none were found");
-    public ResultError Error => _error ?? throw new InvalidOperationException("Error was expected but none were found");
-
-
-    public TResult Match<TResult>(Func<ResultValue, TResult> onValue, Func<ResultError, TResult> onError)
-    {
-        return IsError ? onError(Error) : onValue(Value);
-    }
-
-    public static implicit operator Result(ResultValue value) => new(false, value: value);
-    public static implicit operator Result(ResultError error) => new(true, error: error);
-}
-
-public readonly record struct Result<TValue> : IResult<TValue>
+public readonly struct Result<TValue> : IResult<TValue>
 {
     private readonly TValue? _value;
     private readonly ResultError? _error;
@@ -50,4 +24,30 @@ public readonly record struct Result<TValue> : IResult<TValue>
 
     public static implicit operator Result<TValue>(TValue value) => new(false, value: value);
     public static implicit operator Result<TValue>(ResultError error) => new(true, error: error);
+}
+
+public readonly struct Result : IResult<ResultValue>
+{
+    private readonly ResultValue? _value;
+    private readonly ResultError? _error;
+
+    private Result(bool isError, ResultValue? value = default, ResultError? error = default)
+    {
+        IsError = isError;
+        _error = error;
+        _value = value;
+    }
+
+    public bool IsError { get; }
+    public ResultValue Value => _value ?? throw new InvalidOperationException("Value was expected but none were found");
+    public ResultError Error => _error ?? throw new InvalidOperationException("Error was expected but none were found");
+
+
+    public TResult Match<TResult>(Func<ResultValue, TResult> onValue, Func<ResultError, TResult> onError)
+    {
+        return IsError ? onError(Error) : onValue(Value);
+    }
+
+    public static implicit operator Result(ResultValue value) => new(false, value: value);
+    public static implicit operator Result(ResultError error) => new(true, error: error);
 }
